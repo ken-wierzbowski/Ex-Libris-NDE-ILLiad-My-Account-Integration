cd33851c42e434d644d402b56ce813a52266873d
Ken Wierzbowski <64855242+ken-wierzbowski@users.noreply.github.com>
2025-08-18 16:41:41 -0400

    Add files via upload

---
+++ app/ill/illiad-options.constant.ts
@@ -0,0 +1,7 @@
+export const illiadOptions = {
+  groups: [/* Add Alma user group codes here - ie. 'ALUM','UNDGRDUR'*/],
+  remoteScript: 'https://mydomain.com/illiad.php',
+  boxTitle: 'Interlibrary Loan',
+  illiadURL: 'https://illiad.<Your Institution>/<OCLC SYMBOL>/illiad.dll',
+  apiURL: 'https://illiad.<Your Institution>/ILLiadWebPlatform/Transaction/UserRequests/',
+};
