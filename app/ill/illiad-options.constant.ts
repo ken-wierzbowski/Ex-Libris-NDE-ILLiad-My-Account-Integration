8becf0127630b1df9bd3c14628ab9a54887db24f
Ken Wierzbowski <64855242+ken-wierzbowski@users.noreply.github.com>
2025-09-10 16:39:53 -0400

    Update illiad-options.constant.ts

--- app/ill/illiad-options.constant.ts
+++ app/ill/illiad-options.constant.ts
@@ -3,5 +3,4 @@ export const illiadOptions = {
   remoteScript: 'https://mydomain.com/illiad.php',
   boxTitle: 'Interlibrary Loan',
   illiadURL: 'https://illiad.<Your Institution>/<OCLC SYMBOL>/illiad.dll',
-  apiURL: 'https://illiad.<Your Institution>/ILLiadWebPlatform/Transaction/UserRequests/',
 };
